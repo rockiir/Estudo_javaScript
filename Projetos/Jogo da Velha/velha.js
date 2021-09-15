923477a9c2a0a9ae14489f12b2085cd408bdf67e
rockiir <70121094+rockiir@users.noreply.github.com>
2021-09-14 23:42:31 -0300

    jogo da velha

--- Projetos/Jogo da Velha/velha.js
+++ Projetos/Jogo da Velha/velha.js
@@ -1,6 +1,19 @@
 var jogador =null;
+var jogadorSelecionado = document.getElementById('jogadorSelecionado')
+mudarJogador('x')
+
 function escolherQuadrado(id){
-    console.log(id);
+    var quadrado =  document.getElementById(id)
+    quadrado.innerHTML = jogador
+    quadrado.style.color = '#000'
+
+}
+
+
+function mudarJogador (valor) {
+    jogador = valor;
+    jogadorSelecionado.innerHTML = jogador;
+
 
 }
 
