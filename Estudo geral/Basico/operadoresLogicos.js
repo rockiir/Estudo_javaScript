ce0b0fbb7ab4986291cdf1a057fdf6f8b7208164
rockiir <70121094+rockiir@users.noreply.github.com>
2022-05-25 20:14:41 -0300

    Organizando

---
+++ Estudo geral/Basico/operadoresLogicos.js
@@ -0,0 +1,13 @@
+/* comentarioOperadores lógicos
+
+&& -> AND -> E 
+|| -> OR -> OU
+! -> Not -> Não 
+*/
+
+
+
+const expressaoAnd = true && true && false && true;
+const expressaoOR = false || true ;
+const expressaoNot = 
+console.log(expressaoOR);
