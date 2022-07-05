fdcc2d322b918ade65e42d9878ff08a79bc55b90
rockiir <70121094+rockiir@users.noreply.github.com>
2022-07-04 23:19:24 -0300

    to do http request

--- Projeto Completo NodeJS, React, React Native, TypeScript/segundo-app/src/App.js
+++ Projeto Completo NodeJS, React, React Native, TypeScript/segundo-app/src/App.js
@@ -1,48 +1,56 @@
-import userEvent from "@testing-library/user-event";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [input, setInput] = useState('');
   const [tarefas, setTarefas] = useState([
     'llll'
   ]);
+    useEffect(() => {
+      const tarefasStorage = localStorage.getItem('@tarefa');
+    if(tarefasStorage){
+      setTarefas(JSON.parse(tarefasStorage))
+
+}},[]);
     
-  function handleRegister(e) {
-    e.preventDefault();
+    useEffect(() => {
+      localStorage.setItem('@tarefa', JSON.stringify(tarefas));
+    }, [tarefas]);
 
-    setTarefas([...tarefas,input]);
-    setInput('');
+    function handleRegister(e) {
+      e.preventDefault();
+
+      setTarefas([...tarefas, input]);
+      setInput('');
 
 
-  }
-  return (
-    <div>
-      <h1>Cadastro de usuario</h1>
-      <form onSubmit={handleRegister}>
-        <label >Nome da tarefa:</label><br/>
-        <input type="text" placeholder="Digite uma tarefa"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        /> 
-        <br/> 
-        <button type="submit">registrar</button>
-      </form>
-      <br/><br/>
-      <ul>
-      {tarefas.map(tarefa => (
-        <li>{tarefa}</li>
-      ))}
-
-      </ul>
+    }
+  
+    return (
       <div>
+        <h1>Cadastro de usuario</h1>
+        <form onSubmit={handleRegister}>
+          <label >Nome da tarefa:</label><br />
+          <input type="text" placeholder="Digite uma tarefa"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <br />
+          <button type="submit">registrar</button>
+        </form>
+        <br /><br />
+        <ul>
+          {tarefas.map(tarefa => (
+            <li>{tarefa}</li>
+          ))}
+
+        </ul>
+        <div>
+        </div>
+
+
       </div>
 
-
-    </div>
-
-  );
-}
+    );
+  }
 
 export default App;
-
-
